b1c63aa72e2dc08228c504487fda7ee49dfc57a7
Aleksandr Hrupin <hrupin.work@yandex.ru>
2017-10-23 18:45:58 +0300

    Test settings webhook

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,6 +9,8 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>Congratulations!</h1>
 
+        <h2>Привет</h2>
+
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
